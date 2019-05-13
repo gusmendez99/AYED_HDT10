3d819d36df34ac14fd861973300893c13c36e6ec
Gus Mendez <gus.mendez.99@gmail.com>
2019-05-12 18:58:58 -0600

    Search/Filter queries added

--- hospital.py
+++ hospital.py
@@ -15,6 +15,33 @@ patients = driver.labels.create("Patient")
 doctors = driver.labels.create("Doctor")
 drugs = driver.labels.create("Drug")
 
+# MAIN FUNCION FOR ADD A VISIT
+def registerMedicalVisit(patientName, doctorName, drugName, initDate, endDate, dose):
+    drug = addDrug(drugName, initDate, endDate, dose)
+    linkPatientWithDoctor(patientName, doctorName)
+    linkPatientWithDrug(patientName, drugName)
+    linkDoctorWithDrug(doctorName, drugName)
+
+# SEARCHS
+def filterDoctorBySpecialty(specialty): #filter doctors by specialty, returns a list of doctor names
+    doctors = []
+    query = "MATCH (d:Doctor) WHERE d.specialty = \"{0}\" RETURN d".format(specialty)
+    results = driver.query(query, returns=(client.Node))
+	
+    for node in results:
+        doctors.append(node[0]["name"]) #adds doctor name
+	
+    return doctors
+
+def getKnownPeopleByPatient(patientName): #returns a list of known people (another patients) by current patient
+    knownPeople = []
+    query = 'MATCH (p:Patient)-[r:KNOWS]->(u:Patient) WHERE p.name=\"{0}\" RETURN p, type(r), u'.format(patientName)
+    results = driver.query(query, returns=(client.Node, str, client.Node))
+    
+    for node in results:
+        knownPeople.append(node[2]["name"]) #adds known patient name
+    
+    return knownPeople
 
 # NODES - CREATE
 def addPatient(name, phone):
@@ -32,7 +59,9 @@ def addDrug(name, initDate, endDate, dose):
     drugs.add(drugNode)
     return drugNode
 
-# RELATIONS - CREATE
+
+
+# RELATIONS - MAIN RELATIONSHIPS
 def linkPatientWithDoctor(patientName, doctorName):
     query = "MATCH (p:Patient), (d:Doctor) WHERE p.name=\"{0}\" AND d.name=\"{1}\" RETURN p,d".format(patientName, doctorName)
     results = driver.query(query, returns=(client.Node, client.Node))
@@ -62,3 +91,50 @@ def linkDoctorWithDrug(doctorName, drugName):
         myDoctor.relationships.create("PRESCRIBES", myDrug)
 
 
+# RELATIONS - SECONDARY RELATIONSHIPS
+def linkPatientWithPatient(patientName1, patientName2):
+    queryPatient1 ='MATCH (p:Patient) WHERE p.name=\"{0}\" RETURN p'.format(patientName1)
+    results = driver.query(queryPatient1,returns=(client.Node))
+
+    for node in results:
+        myPatient1 = node[0]
+
+    queryPatient2 ='MATCH (p:Patient) WHERE p.name=\"{0}\" RETURN p'.format(patientName2)
+    results = driver.query(queryPatient2,returns=(client.Node))
+
+    for node in results:
+        myPatient2 = node[0]
+        myPatient1.relationships.create("KNOWS", myPatient2)
+  
+
+def linkDoctorWithPatient(doctorName, patientName):
+    query ='MATCH (p:Paciente) WHERE p.name= \"{0}\" RETURN p'.format(patientName)
+    results = driver.query(query,returns=(client.Node))
+    
+    for node in results:
+        myPatient = node[0]
+
+    query='MATCH (d:Doctor) WHERE d.name=\"{0}\" RETURN d'.format(doctorName)
+    results = driver.query(query,returns=(client.Node))
+    for node in results:
+        myDoctor = node[0]
+        myDoctor.relationships.create("KNOWS", myPatient)
+  
+
+def linkDoctorWithDoctor(doctorName1, doctorName2):
+    query ='MATCH (d:Doctor) WHERE d.name=\"{0}\" RETURN d'.format(doctorName1)
+    results = driver.query(query,returns=(client.Node))
+    for node in results:
+        myDoctor1 = node[0]
+
+    query ='MATCH (d:Doctor) WHERE d.name=\"{0}\" RETURN d'.format(doctorName2)
+    results = driver.query(query,returns=(client.Node))
+    for node in results:
+        myDoctor2=node[0]
+
+    myDoctor1.relationships.create("KNOWS", myDoctor2)
+
+
+
+
+# RECOMMENDATION SYSTEM
