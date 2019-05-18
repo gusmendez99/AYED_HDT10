717a378fb342f112af57af055fb47c26f68c7e23
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-05-17 19:45:52 -0600

    Cambios en la data
    En este commit agrego cambios en al informacion del grafo

--- dummyCreator.py
+++ dummyCreator.py
@@ -1,1 +1,32 @@
 #Here we're gonna add dummy data, just for tests
+
+from hospital import *
+
+#Agrega doctores al grafo
+
+
+
+d1 = addDoctor("Juan","25841644","5021","Cardiologo")
+addDoctor("Carlos","57875435","3301","Dermatologo")
+addDoctor("Pedro","97895456","2255","Otorrinolaringologo")
+addDoctor("Diego","78250828","5566","Neurocirijano")
+addDoctor("Francisco","47345855","6699","Anestiologo")
+addDoctor("Michael","82185063","5584","Cirujano")
+addDoctor("Saul","25841644","6551","Urologia")
+addDoctor("Luis","40435145","8874","Ginecologo")
+
+#Agrega pacientes al grafo
+
+addPatient("Steven","68946194")
+addPatient("Maria","29193929")
+addPatient("Zonia","45558785")
+addPatient("Ines","49026128")
+
+
+#Crea relaciones
+registerMedicalVisit("Steven","Juan","Alka-Seltzer","1 c/d 7 hrs")
+registerMedicalVisit("Maria","Carlos","Panadol","3 c/d 7 hrs")
+registerMedicalVisit("Zonia","Francisco","Amoxicilina","2 c/d 7 hrs")
+
+
+
